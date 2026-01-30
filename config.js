8fbdaf3044c81324f3ff2debd7c7436c3b08637b
chimiqueen <chimi-d@live.de>
2026-01-30 22:01:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,7 @@ const CONFIG = {
     loveMessages: {
         extreme: "WOOOOW You love me that much?? 🥹",  // Shows when they go past 5000%
         high: "This is alotttt 🥰",              // Shows when they go past 1000%
-        normal: "Okay, but could be more if you ask me 👀"                           // Shows when they go past 100%
+        normal: "Sould be more if you ask me 👀"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
